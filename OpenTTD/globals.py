5366c12dc3e283ec79164207f4d97603458727e4
NahashonM <mwongeranahashon@gmail.com>
2024-01-13 12:27:21 +0300

    added comments for globals

--- OpenTTD/globals.py
+++ OpenTTD/globals.py
@@ -1,10 +1,25 @@
 
-
+# global bot for discord communication and control
+#-------------------------------------------------
 discord_bot = None
+
+
+# Global OpenTTD admin clients 
+#     poll admin : updates on query
+#  update admin  : automatic updates
+#-------------------------------------------------
 ottdPollAdmin = None
 ottdUpdateAdmin = None
 
+
+# Global OpenTTD structures 
+#      ottd_clients : players currently in the game
+#    ottd_companies : companies currently in the game
+#------------------------------------------------- 
 ottd_clients = {}
+ottd_companies = {}
 
+
+#
+#-------------------------------------------------
 serverWelcome = None
-
